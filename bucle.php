90572035f47309300f585bfb85b15b5d860d263f
Gisela Arena <49735736+Gise01@users.noreply.github.com>
2019-04-26 22:01:18 -0300

    Clase

---
+++ bucle.php
@@ -0,0 +1,34 @@
+<?php
+
+for ($i=0; $i<=10; $i++) {
+    echo 'El resultado de ' ."$i" .' * 2 es ' .$i*2 .'<br>';
+} 
+?>
+
+<?php 
+
+$i=100;
+while ($i>84) {
+    echo $i .'<br>';
+    $i--;
+}
+?>
+
+<?php
+$i=1;
+while ($i<=5){
+    echo $i*2 .'<br>';
+    $i++;
+}
+?>
+
+<?php 
+
+if
+while ($i=1) {
+    echo 1;
+    $i++
+
+}
+?>
+
